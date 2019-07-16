f37f85f4fc1dd1758a37285f557682fcd129d499
Graham Fairweather <xotic750@gmail.com>
2019-07-17 00:19:39 +0200

    Terraformed
    Auto build

--- __tests__/error-x.test.js
+++ __tests__/error-x.test.js
@@ -1,28 +1,5 @@
 let lib;
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-  lib = require('../../index.js');
-} else {
-  lib = returnExports;
-}
-
 describe('error-x', function() {
   describe('standard error type', function() {
     it('should not throw an error with bad arguments', function() {
